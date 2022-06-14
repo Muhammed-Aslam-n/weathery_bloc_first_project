83caba7ebd0a69152fe3035ae678042932bff168
Muhammed-Aslam-n <muhammedaslamn210@gmail.com>
2022-06-14 17:09:08 +0530

    second commit - keyChanges

--- lib/services/api_service/api_constants/api_constants.dart
+++ lib/services/api_service/api_constants/api_constants.dart
@@ -1,4 +1,4 @@
 class ApiConstants{
   static const String? baseUrl = "https://api.openweathermap.org/data/2.5/weather?q=";
-  static const String? weatherApiKey =  "c93a4ff7e84179e2a2efcc42aa09e3cb";
+  static const String? weatherApiKey =  "";
 }
